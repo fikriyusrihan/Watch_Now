f823a7fabdbf0d4ad161fe3d0326e64f16bd9d20
Fikri Yusrihan <yusrihanfikri@gmail.com>
2020-08-13 12:03:32 +0700

    Unit & UI Test Complete Submission 1 Complete

--- app/src/androidTest/java/com/artwork/space/watchnow/ui/movie/MovieFragmentTest.kt
+++ app/src/androidTest/java/com/artwork/space/watchnow/ui/movie/MovieFragmentTest.kt
@@ -1,15 +1,67 @@
 package com.artwork.space.watchnow.ui.movie
 
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.ActivityTestRule
+import com.artwork.space.watchnow.R
+import com.artwork.space.watchnow.utils.DataDummy
+import com.builder.watch.testing.SingleFragmentActivity
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 class MovieFragmentTest {
+    private val dummyData = DataDummy.generateDummyMovie()
+    private val dummyDataAtFirst = dummyData[0]
+
     @get:Rule
-    val movieFragment = MovieFragment()
+    val activityRule = ActivityTestRule(SingleFragmentActivity::class.java)
+    private val movieFragment = MovieFragment()
 
     @Before
     fun setUp() {
+        activityRule.activity.setFragment(movieFragment)
+    }
+
+    @Test
+    fun isAllDisplayed() {
+        onView(withId(R.id.fragment_movie_main_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.fragment_movie_secondary_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.main_movies_recycler_view)).check(matches(isDisplayed()))
+        onView(withId(R.id.main_movies_recycler_view)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                dummyData.size
+            )
+        )
+    }
+
+    @Test
+    fun clickFirstElementInRecyclerView() {
+        onView(withId(R.id.main_movies_recycler_view)).check(matches(isDisplayed()))
+        onView(withId(R.id.main_movies_recycler_view)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+
+        onView(withId(R.id.detail_movie_tv_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.detail_movie_tv_title)).check(matches(withText(dummyDataAtFirst.title)))
+
+        onView(withId(R.id.detail_movie_tv_release_date)).check(matches(isDisplayed()))
+        onView(withId(R.id.detail_movie_tv_release_date)).check(matches(withText(dummyDataAtFirst.releaseDate)))
+
+        onView(withId(R.id.detail_movie_tv_description)).check(matches(isDisplayed()))
+        onView(withId(R.id.detail_movie_tv_description)).check(matches(withText(dummyDataAtFirst.description)))
+
+        onView(withId(R.id.detail_movie_btn_back)).check(matches(isDisplayed()))
+        onView(withId(R.id.detail_movie_iv_poster)).check(matches(isDisplayed()))
+        onView(withId(R.id.detail_movie_iv_rating)).check(matches(isDisplayed()))
+
     }
 
 }
---
+++ app/src/androidTest/java/com/artwork/space/watchnow/ui/tvshow/TVShowFragmentTest.kt
@@ -0,0 +1,86 @@
+package com.artwork.space.watchnow.ui.tvshow
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.rule.ActivityTestRule
+import com.artwork.space.watchnow.R
+import com.artwork.space.watchnow.utils.DataDummy
+import com.builder.watch.testing.SingleFragmentActivity
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class TVShowFragmentTest {
+    private val dummyData = DataDummy.generateDummyTvShow()
+    private val dummyDataAtFirst = dummyData[0]
+
+    @get:Rule
+    val activityRule = ActivityTestRule(SingleFragmentActivity::class.java)
+    private val tvShowFragment = TVShowFragment()
+
+    @Before
+    fun setUp() {
+        activityRule.activity.setFragment(tvShowFragment)
+    }
+
+    @Test
+    fun isAllDisplayed() {
+        onView(withId(R.id.fragment_tv_main_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.fragment_tv_secondary_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.main_tv_recycler_view)).check(matches(isDisplayed()))
+        onView(withId(R.id.main_tv_recycler_view)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                dummyData.size
+            )
+        )
+    }
+
+    @Test
+    fun clickFirstElementInRecyclerView() {
+        onView(withId(R.id.main_tv_recycler_view)).check(matches(isDisplayed()))
+        onView(withId(R.id.main_tv_recycler_view)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                ViewActions.click()
+            )
+        )
+
+        onView(withId(R.id.detail_tv_tv_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.detail_tv_tv_title)).check(
+            matches(
+                ViewMatchers.withText(
+                    dummyDataAtFirst.title
+                )
+            )
+        )
+
+        onView(withId(R.id.detail_tv_tv_release_date)).check(matches(isDisplayed()))
+        onView(withId(R.id.detail_tv_tv_release_date)).check(
+            matches(
+                ViewMatchers.withText(
+                    dummyDataAtFirst.releaseDate
+                )
+            )
+        )
+
+        onView(withId(R.id.detail_tv_tv_description)).check(matches(isDisplayed()))
+        onView(withId(R.id.detail_tv_tv_description)).check(
+            matches(
+                ViewMatchers.withText(
+                    dummyDataAtFirst.description
+                )
+            )
+        )
+
+        onView(withId(R.id.detail_tv_btn_back)).check(matches(isDisplayed()))
+        onView(withId(R.id.detail_tv_iv_poster)).check(matches(isDisplayed()))
+        onView(withId(R.id.detail_tv_iv_rating)).check(matches(isDisplayed()))
+
+    }
+}
